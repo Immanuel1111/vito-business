6793ce59c89e1d436df90bf0f70e67df402a4cf3
Immanuel1111 <Immans211@gmail.com>
2026-02-28 14:55:16 +0530

    28 14:55

--- vito/src/pages/Home/Home.jsx
+++ vito/src/pages/Home/Home.jsx
@@ -3,7 +3,9 @@ import "./Home.css";
 import bgVideo from "./Assets/Video/video1.mp4";
 import aboutImg from "./Assets/Video/video2.gif"; // add your image
 import Navbar from "../../components/Navbar/Navbar";
-
+import service1 from "./Assets/images/1.avif"
+import service2 from "./Assets/images/2.avif"
+import service3 from "./Assets/images/3.avif"
 function Home() {
   return (
     <>
@@ -64,7 +66,63 @@ function Home() {
           </div>
         </div>
       </div>
-      
+      {/* NEXT DIV - Services Section */}
+      <div className="services-section">
+        <div className="services-container">
+
+          <h2 className="services-title">
+            Our <span>Services</span>
+          </h2>
+
+          <p className="services-subtitle">
+            We supply high quality scrap steel for various industrial applications,
+            ensuring responsible sourcing and processing. Our extensive network allows
+            us to offer competitive pricing and reliable delivery to meet your project needs.
+          </p>
+
+          <div className="services-grid">
+
+            {/* Service 1 */}
+            <div className="service-card">
+              <div className="service-circle">
+                <img src={service1} alt="Maintenance services" />              </div>
+              <h3>Maintenance services</h3>
+              <p>
+                At Subra Metals, a range of essential maintenance tasks is undertaken
+                to ensure that equipment, facilities, and systems operate at peak performance.
+              </p>
+            </div>
+
+            {/* Service 2 */}
+            <div className="service-card">
+              <div className="service-circle">
+                <img src={service2} alt="Scrap Metal Supply" />
+              </div>
+              <h3>Scrap Metal Supply</h3>
+              <p>
+                At Subra Metals, we provide an extensive selection of scrap metals
+                tailored for various industrial applications and projects.
+              </p>
+            </div>
+
+            {/* Service 3 */}
+            <div className="service-card">
+              <div className="service-circle">
+                <img src={service3} alt="scrap Metal Services" />
+              </div>
+              <h3>Scrap Metal Services</h3>
+              <p>
+                Our scrap metal services include a full range of offerings designed
+                to make recycling easy and efficient for businesses and individuals alike.
+              </p>
+            </div>
+
+          </div>
+
+          <button className="services-btn">Know More</button>
+        </div>
+      </div >
+
     </>
   );
 }
