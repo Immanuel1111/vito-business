e4b83f65b90ea07d4fc84f4831c04e1e5274575c
Immanuel1111 <Immans211@gmail.com>
2026-02-28 14:35:00 +0530

    28 14:34

--- vito/src/pages/Home/Home.jsx
+++ vito/src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Home.css";
 import bgVideo from "./Assets/Video/video1.mp4";
+import aboutImg from "./Assets/Video/video2.gif"; // add your image
 import Navbar from "../../components/Navbar/Navbar";
 
 function Home() {
@@ -9,8 +10,8 @@ function Home() {
       {/* Navbar on top */}
       <Navbar />
 
+      {/* Hero Section */}
       <div className="hero-container">
-        {/* Background Video */}
         <video
           className="hero-video"
           autoPlay
@@ -22,10 +23,8 @@ function Home() {
           Your browser does not support the video tag.
         </video>
 
-        {/* Dark Overlay */}
         <div className="hero-overlay"></div>
 
-        {/* Front Content */}
         <div className="hero-content">
           <h1 className="hero-title">
             Your Sustainable Scrap Metal <br /> Recycling Partner
@@ -38,6 +37,31 @@ function Home() {
           </p>
 
           <button className="hero-btn">Learn More</button>
+        </div>
+      </div>
+
+      {/* NEXT DIV - About Section */}
+      <div className="about-section">
+        <div className="about-container">
+          <div className="about-text">
+            <h2>
+              About <span>VITO</span>Business Solutions Pty Ltd
+            </h2>
+
+            <p>
+              Vito Business Solutions pty Ltd is a leading provider of sustainable scrap metal
+              recycling, including the supply of scrap steel, and industrial
+              maintenance services. Our dedicated team ensures that every
+              project, whether small or large, receives the highest level of
+              professionalism, efficiency, and environmental care.
+            </p>
+
+            <button className="about-btn">Learn more</button>
+          </div>
+
+          <div className="about-image">
+            <img src={aboutImg} alt="Subra Metals" />
+          </div>
         </div>
       </div>
       
