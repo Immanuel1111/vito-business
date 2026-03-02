650023b41080defc5a944c50196993aff1125e86
Immanuel1111 <Immans211@gmail.com>
2026-03-02 12:50:26 +0530

    02 12:50

--- vito/src/App.jsx
+++ vito/src/App.jsx
@@ -1,11 +1,15 @@
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import React from "react";
 import Home from "./pages/Home/Home.jsx";
-
+import About from "./pages/Aboutus/About.jsx"
 function App() {
   return (
-    <div>
-      <Home />
-    </div>
+   <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
   );
 }
 
--- vito/src/pages/Aboutus/About.jsx
+++ vito/src/pages/Aboutus/About.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from 'react';
+import Navbar from "../../components/Navbar/Navbar";
+import "./About.css";
+import aboutHero from "./images/1.webp";
 
 const About = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <>
+      <Navbar />
 
-export default About
+      <div className="about-hero">
+        <img src={aboutHero} alt="About Hero" className="about-hero-bg" />
+        <div className="about-hero-overlay"></div>
+        <div className="about-hero-content">
+          <h1>About Us</h1>
+          <p>Subra Metals: Your trusted partner for premium scrap metal recycling and eco-friendly solutions.</p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default About;
